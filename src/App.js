105637851ee75ba0fd9fb0c6466a440e616b37af
chris <12405301@qq.com>
2018-04-09 00:02:14 +0800

    side menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Sidebar from 'react-sidebar';
+
+const mql = window.matchMedia(`(min-width: 800px)`);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Link to="/step1"> step1 </Link>
-        <Link to="/home"> home </Link>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mql: mql,
+            docked: props.docked,
+            open: props.open
+        }
+
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({sidebarOpen: open});
+    }
+
+    componentWillMount() {
+        mql.addListener(this.mediaQueryChanged);
+        this.setState({mql: mql, sidebarDocked: mql.matches});
+    }
+
+    componentWillUnmount() {
+        this.state.mql.removeListener(this.mediaQueryChanged);
+    }
+
+    mediaQueryChanged() {
+        this.setState({sidebarDocked: this.state.mql.matches});
+    }
+    render() {
+        var sidebarContent = <b>Sidebar content</b>;
+        var sidebarProps = {
+            sidebar: this.state.sidebarOpen,
+            docked: this.state.sidebarDocked,
+            onSetOpen: this.onSetSidebarOpen
+        };
+        return (
+            <Sidebar sidebar={sidebarContent}
+                     open={this.state.sidebarOpen}
+                     docked={this.state.sidebarDocked}
+                     onSetOpen={this.onSetSidebarOpen}>
+                <div className="content">
+                    <Link className="link" to="/step1"> step1 </Link>
+                    <Link className="link" to="/home"> home </Link>
+                </div>
+            </Sidebar>
+
+        );
+    }
 }
 
 export default App;
